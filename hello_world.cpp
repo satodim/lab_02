f86f70b391d8b886cd99073695d57d6df6cae7a3
satodim <vovkakursk8@gmail.com>
2026-03-09 13:55:47 +0000

    changed code style

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,13 +1,14 @@
 // Подключаем библиотеку ввода-вывода
 #include <iostream>
 
-int main() {
-	// Создаем строку для имени
-	std::string name;
-	// Принимаем имя
-	std::cin >> name;
-	// Печатаем
-	std::cout << "Hello world from " << name << std::endl;
-	return 0;
+int
+main()
+{
+  // Создаем строку для имени
+  std::string name;
+  // Принимаем имя
+  std::cin >> name;
+  // Печатаем
+  std::cout << "Hello world from " << name << std::endl;
+  return 0;
 }
-
